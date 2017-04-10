136368148e0c3005a4a9f9cca7113250630cad3d
castro30 <castro30@yahoo.com>
2017-04-10 17:28:53 -0500

    Initial Commit

---
+++ EvenOrOddDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOddDemo
+{
+    class GetInputandCalculate
+    {
+        public void userInput(int usernumber)
+        {
+            if (usernumber % 2 == 0)
+            {
+                Console.WriteLine(usernumber+ " Number is even!!");
+            }
+            else Console.WriteLine(usernumber+ " Number is odd!");
+            
+        }
+    }
+    class Program
+    {
+        /* Write a program that takes in an array of numbers and 
+         * Outputs whether the are even or odd.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the amount of integers to be added to the array : ");
+
+            string testInput = Console.ReadLine();
+            String[] substrings = testInput.Split(' ');
+            int[] myInts = Array.ConvertAll(substrings, int.Parse);
+            
+            // Looping the customers input into an array
+           
+
+            GetInputandCalculate guess = new GetInputandCalculate();
+
+
+            System.Collections.ArrayList l = new System.Collections.ArrayList();
+            for (int a = 0; a < myInts.Length; a++)
+            {
+            guess.userInput(myInts[a]);
+            }
+            for (int k = 0; k < l.Count; k++)
+            {
+                Console.Write((int)l[k] + (int)l[k] + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
